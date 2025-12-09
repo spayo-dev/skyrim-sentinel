8846cce0755f7383eb41aa1dd58b99d346cc5d08
Sean Payomo <206040970+spayo-dev@users.noreply.github.com>
2025-12-08 23:03:02 -0700

    feat: demo

---
+++ demo/demo.py
@@ -0,0 +1,19 @@
+import hashlib
+
+# Data to be hashed (must be bytes)
+data = b"This is a."
+
+# Create a SHA256 hash object
+sha256_hash = hashlib.sha256()
+
+# Update the hash object with the data
+sha256_hash.update(data)
+
+# Get the hexadecimal representation of the hash
+hex_digest = sha256_hash.hexdigest()
+
+print(f"SHA256 hash of '{data.decode()}': {hex_digest}")
+
+# You can also do this in a more condensed way:
+md5_digest = hashlib.md5(b"Another string to hash").hexdigest()
+print(f"MD5 hash of 'Another string to hash': {md5_digest}")
